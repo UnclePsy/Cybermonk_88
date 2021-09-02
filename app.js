6bf75bd4c446fd6680ed45dc31c007fdf8693c61
Nikkolas Jackson <55883796+NikkolasJ@users.noreply.github.com>
2021-09-02 10:55:15 -0400

    Updated app.js
    I changed the formatting of the JavaScript file by moving certain parts to different places.  I added a starter location: http://localhost:8008/ for your express app.  I changed it so your using JSON instead of normal send because you have express.json() for your app.  Changed the way you get your port. Add the port to your .env file. Check line 32 for more info. If you cannot see it when testing download: https://chrome.google.com/webstore/detail/json-viewer/gbmdgpbipfallnflgajpaliibnhdgobh if you are using chrome/brave.  Everything is using arrow functions because that is the way of the JavaScripter. I suggest installing morgan dependency.  It allows you to see requests from your site and your requests to api's/sites. The code for morgan is added already just uncomment them if you want to use it if not delete it. They are on lines 26, 27, 28, and 36.

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-var onMessageHandler = require("./command");
-var onRedeemHandler = require('./redeem');
-var client = require("./client");
-var express = require("express");
-var app = express();
-const port = 8800;
+// Twitch Packages
+const onMessageHandler = require("./command");
+const onRedeemHandler = require('./redeem');
+const client = require("./client");
+
+// Adds env variables
 require("dotenv").config();
+
 //Twitch//
 // Register our event handlers (defined below)
 client.on("message", onMessageHandler);
@@ -15,18 +16,39 @@ client.on('redeem', onRedeemHandler);
 client.connect();
 
 // Called every time the bot connects to Twitch chat
-function onConnectedHandler(addr, port) {
+let onConnectedHandler = (addr, port) => {
     console.log(`* Connected to ${addr}:${port}`);
 }
 
-//PubSub
+
+// Express Packages
+const express = require("express");
+// Uncomment morgan stuff if you decide to use it: Lines: 26, 27, and 36. Also if you want info check info link
+//const morgan = require("morgan");  // Yarn add morgan --dev or npm i -d morgan
+// info here: https://www.npmjs.com/package/morgan
+
+// Express App
+const app = express();
+const port = process.env.EXPRESS_PORT || 8008; // Add the port to the .env file called with a variable called EXPRESS_PORT
+
+//Express Dependencies 
 app.use(express.json());
-app.get('/sub/youtube', function(req, res) {
-    res.send('Thank you');
-    console.log("It worked");
+//app.use(morgan('tiny')); // Uncomment if you decide to use morgan
+
+// Express Starter page: http://localhost:8008/
+app.get('/', (req, res) => {
+    res.json({ message: 'Hello, World' });
+}
+
+// Express subsciption page: http://localhost:8008/sub/youtube/      
+app.get('/sub/youtube', (req, res) => {
+    res.json({ message: 'Thank you' });
+    console.log('Request Information:');
     console.log(req.headers);
     console.log(req.body);
-})
+});
+
+// Starts to listen for the port
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
-})
+});
